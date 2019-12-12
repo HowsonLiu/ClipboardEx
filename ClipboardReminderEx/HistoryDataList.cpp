58db06c0698e7b8e0853750e0053b93bd732629a
HowsonLiu <948470636@qq.com>
2019-12-12 20:01:16 +0800

    read clipboard data in another thread

--- ClipboardReminderEx/HistoryDataList.cpp
+++ ClipboardReminderEx/HistoryDataList.cpp
@@ -3,13 +3,45 @@
 #include <QClipboard>
 #include <QMimeData>
 #include <QUrl>
+#include <QTime>
+#include <QTimer>
+#include <QDebug>
+
+namespace {
+	static const int g_readMaxTime = 5 * 1000;
+}
 
 ClipboardData::ClipboardData(const QMimeData* mimedata)
 {
-	if (mimedata->hasImage()) image = qvariant_cast<QImage>(mimedata->imageData());
+	if (mimedata->hasImage()) image = qvariant_cast<QImage>(mimedata->imageData()).copy();
 	else if (mimedata->hasText()) text = mimedata->text();
 	else if (mimedata->hasHtml()) text = mimedata->html();
 	else if (mimedata->hasUrls()) urls = mimedata->urls();
+}
+
+void ClipboardData::clear()
+{
+	image = QImage();
+	text.clear();
+	urls.clear();
+}
+
+void ReadClipboardThread::run()
+{
+	QTime startTime = QTime::currentTime();
+	m_data.clear();
+	while (startTime.msecsTo(QTime::currentTime()) < g_readMaxTime  && !m_data.isValid()) {
+		// m_data = ClipboardData(QApplication::clipboard()->mimeData());
+		this->sleep(0);
+	}
+	if (m_data.isValid()) {
+		emit sigSuccessed(m_data);
+		qDebug() << "success";
+	}
+	else {
+		emit sigFailed();
+		qDebug() << "failed";
+	}
 }
 
 HistoryDataList* HistoryDataList::getInstance()
@@ -20,8 +52,10 @@ HistoryDataList* HistoryDataList::getInstance()
 
 HistoryDataList::HistoryDataList(QObject* parent) : QObject(parent)
 {
+	m_thread = new ReadClipboardThread(this);
 	m_historyClipboardDataList.push_front(std::make_shared<ClipboardData>(QApplication::clipboard()->mimeData()));
-	connect(QApplication::clipboard(), &QClipboard::dataChanged, getInstance(), &HistoryDataList::onClipboardDataUpdate);
+	connect(m_thread, &ReadClipboardThread::sigSuccessed, this, &HistoryDataList::onClipboardDataUpdate);
+	connect(QApplication::clipboard(), &QClipboard::dataChanged, [this]() { m_thread->start(); });
 }
 
 HistoryDataList::~HistoryDataList()
@@ -38,7 +72,7 @@ void HistoryDataList::onSetListSize(int s)
 	m_listSize = s;
 }
 
-void HistoryDataList::onClipboardDataUpdate()
+void HistoryDataList::onClipboardDataUpdate(const ClipboardData& data)
 {
 	while (m_historyClipboardDataList.size() >= m_listSize)
 		m_historyClipboardDataList.pop_back();
--- ClipboardReminderEx/HistoryDataList.h
+++ ClipboardReminderEx/HistoryDataList.h
@@ -4,8 +4,10 @@
 #include <QString>
 #include <deque>
 #include <QUrl>
+#include <QThread>
 
 class QMimeData;
+class QTimer;
 
 /*!
  * \class ClipboardData
@@ -23,6 +25,22 @@ struct ClipboardData {
 	inline bool hasUrls() const { return !urls.isEmpty(); }
 	inline bool hasImage() const { return !image.isNull(); }
 	inline bool hasText() const { return !text.isEmpty(); }
+	inline bool isValid() const { return hasUrls() || hasImage() || hasText(); }
+	void clear();
+};
+
+class ReadClipboardThread : public QThread 
+{
+	Q_OBJECT
+public:
+	explicit ReadClipboardThread(QObject* parent = nullptr) {};
+protected:
+	virtual void run() override;
+signals:
+	void sigSuccessed(const ClipboardData& data);
+	void sigFailed();
+private:
+	ClipboardData m_data;
 };
 
 class HistoryDataList : public QObject
@@ -31,23 +49,19 @@ class HistoryDataList : public QObject
 public:
 	static HistoryDataList* getInstance();
 	inline auto dataList() const { return &m_historyClipboardDataList; }
-
 private:
 	HistoryDataList(QObject* parent = nullptr);
 	~HistoryDataList();
-
 signals:
 	// called when resize the list or clipboard update
 	void sigDataListUpdate();
-
 public slots:
 	void onSetListSize(int);
-
 private slots:
-	void onClipboardDataUpdate();
-
+	void onClipboardDataUpdate(const ClipboardData& data);
 private:
 	// older data are in the end of the list
 	std::deque<std::shared_ptr<ClipboardData>> m_historyClipboardDataList;
 	int m_listSize;
+	ReadClipboardThread* m_thread;
 };
