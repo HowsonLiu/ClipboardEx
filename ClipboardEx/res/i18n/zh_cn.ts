956608bd9672b65cfb270cd2d96e68960078dec6
HowsonLiu <948470636@qq.com>
2020-06-13 19:20:48 +0800

    update i18n

--- ClipboardEx/res/i18n/zh_cn.ts
+++ ClipboardEx/res/i18n/zh_cn.ts
@@ -4,7 +4,7 @@
 <context>
     <name>ClipboardTipsWindow</name>
     <message>
-        <location filename="../../ClipboardTipsWindow.cpp" line="224"/>
+        <location filename="../../ClipboardTipsWindow.cpp" line="129"/>
         <source>Auto hide</source>
         <translation>自动隐藏</translation>
     </message>
@@ -12,29 +12,57 @@
 <context>
     <name>MainControl</name>
     <message>
-        <location filename="../../MainControl.cpp" line="80"/>
+        <location filename="../../MainControl.cpp" line="89"/>
         <source>Start up</source>
         <translation>开机启动</translation>
     </message>
     <message>
-        <location filename="../../MainControl.cpp" line="85"/>
+        <location filename="../../MainControl.cpp" line="94"/>
+        <source>Show Time</source>
+        <translation>显示时长</translation>
+    </message>
+    <message>
+        <location filename="../../MainControl.cpp" line="100"/>
         <source>History Size</source>
         <translation>历史容量</translation>
     </message>
     <message>
-        <location filename="../../MainControl.cpp" line="91"/>
+        <location filename="../../MainControl.cpp" line="106"/>
         <source>Num of tips</source>
         <translation>提示窗口数量</translation>
     </message>
     <message>
-        <location filename="../../MainControl.cpp" line="98"/>
+        <location filename="../../MainControl.cpp" line="113"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../../MainControl.cpp" line="108"/>
+        <location filename="../../MainControl.cpp" line="124"/>
         <source>is enabled</source>
         <translation>已启用</translation>
+    </message>
+</context>
+<context>
+    <name>MimeDataLabel</name>
+    <message>
+        <location filename="../../mimedatalabel.cpp" line="113"/>
+        <source>Save as...</source>
+        <translation>另存为...</translation>
+    </message>
+    <message>
+        <location filename="../../mimedatalabel.cpp" line="137"/>
+        <source>Save as</source>
+        <translation>另存为</translation>
+    </message>
+    <message>
+        <location filename="../../mimedatalabel.cpp" line="146"/>
+        <source>Save error</source>
+        <translation>保存失败</translation>
+    </message>
+    <message>
+        <location filename="../../mimedatalabel.cpp" line="146"/>
+        <source>Can&apos;t open file</source>
+        <translation>打开文件失败，请检查是否被占用</translation>
     </message>
 </context>
 <context>
